76bdf4db0c5182f9e423ed98ca214735b34fe144
“delfinapazzavalia” <delfinapazzavalia@gmail.com>
2023-12-11 10:47:04 -1000

    main page

--- src/components/sections/about/About.jsx
+++ src/components/sections/about/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
                 Especialidad : SISTEMAS DE BOMBEO DE FLUIDOS Y BOMBAS CENTRIFUGAS "(KSB ARGENTINA)" 
                 </p>
                 <p className="text">
-                Desde 1985 amplia experiencia en el ámbito de la ingeniería, mantenimiento y servicios en la industria 
+                Desde 1985 amplia experiencia en el ámbito de la ingeniería, mantenimiento y servicios en la industria. 
 Actualmente y desde el año 2004 brindando servicios de CONSULTORIA Y CAPACITACION sobre SISTEMAS DE BOMBEO Y BOMBAS CENTRIFUGAS a muchas e importantes empresas industriales con sus procesos con sistemas de bombeo centrifugo, de diversos segmentos : minería, petróleo, quimicas, petroquimicas, centrales termoeléctricas, agroindustrias azucarera, alcohol, citrícolas, alimenticias, etc.
                 </p>
             </div>
