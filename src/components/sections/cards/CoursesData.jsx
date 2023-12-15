922c66f99a87ecb5ebb79bb307fdd1072ca7c48b
“delfinapazzavalia” <delfinapazzavalia@gmail.com>
2023-12-14 15:05:36 -1000

    cardCourses

--- src/components/sections/cards/CoursesData.jsx
+++ src/components/sections/cards/CoursesData.jsx
@@ -1,5 +1,5 @@
 const CoursesData = [
-    { id: 1, titulo: 'Tarjeta 1', contenido: 'Contenido de la tarjeta 1' },
+    { id: 1, titulo: 'Cursos', contenido: 'Contenido de la tarjeta 1' },
     { id: 2, titulo: 'Tarjeta 2', contenido: 'Contenido de la tarjeta 2' },
     // Agrega más datos según sea necesario
   ];
