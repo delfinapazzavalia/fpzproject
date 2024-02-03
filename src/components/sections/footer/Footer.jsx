3f98d76af825358444fcb8a3d135f9c6c946cf23
delfinapazzavalia <delfinapazzavalia@gmail.com>
2024-02-02 21:46:27 -1000

    footer added

--- src/components/sections/footer/Footer.jsx
+++ src/components/sections/footer/Footer.jsx
@@ -4,9 +4,17 @@ import "../footer/footer.css"
 const Footer = () => {
     return (
         <>
-            <div>FOOTER</div>
+            <footer className="footer">
+                <div className="footer-content">
+                    <h3 className="footer-title">Get in Touch</h3>
+                    <div className="social-links">
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/delfinapazzavalia">GitHub</a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/delfina-paz-zaval%C3%ADa-9282a31ba/">LinkedIn</a>
+                    </div>
+                    <p>© 2024 Created by Delfina Paz Zavalia.</p>
+                </div>
+            </footer>
         </>
-
     )
 }
 
