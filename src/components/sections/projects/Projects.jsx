82bbc4d91d5f5b6ac30267b655735662a385f841
delfinapazzavalia <delfinapazzavalia@gmail.com>
2024-02-06 18:05:20 -1000

    projects

--- src/components/sections/projects/Projects.jsx
+++ src/components/sections/projects/Projects.jsx
@@ -4,7 +4,14 @@ import "../projects/projects.css"
 const Projects = () => {
     return (
         <>
-            <div>PROJECTS</div>
+            <div id='Projects' className='projects'>
+                <h2>PROJECTS</h2>
+                <div className='projects-cards'>
+                    <Travel />
+                    <Movies />
+                    <Gym />
+                </div>
+            </div>
         </>
 
     )
