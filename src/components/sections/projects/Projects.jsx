e4152cfc541de6a378c63d740d51c8292b7fe0ee
delfinapazzavalia <delfinapazzavalia@gmail.com>
2024-02-13 21:45:05 -1000

    projects updated

--- src/components/sections/projects/Projects.jsx
+++ src/components/sections/projects/Projects.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import "../projects/projects.css"
+import React from "react";
+import "../projects/projects.css";
+import CardProj from "../projects/cardsProjects/CardProj"
 
 const Projects = () => {
-    return (
-        <>
-            <div id='Projects' className='projects'>
-                <h2>PROJECTS</h2>
-                <div className='projects-cards'>
-        
-                </div>
-            </div>
-        </>
+  return (
+    <>
+      <div id="Projects" className="projects">
+        <h2>PROJECTS</h2>
+        <div className="projects-cards">
+          <CardProj />
+        </div>
+      </div>
+    </>
+  );
+};
 
-    )
-}
-
-export default Projects
+export default Projects;
---
+++ src/components/sections/projects/cardsProjects/CardProj.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "../cardsProjects/cardStyle.css"
+
+const CardProj = () => {
+  return (
+    <>
+    <div className="card">
+      
+      <div className="card-content">
+        <h3 className="card-title">Proyectos</h3>
+        <p className="card-description">freelance</p>
+        <p className="card-info">2024</p>
+        <a className="card-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/drive/folders/19HnkhsGSMxEFsRkms9pnC0jDCsBaKl6u?usp=sharing">
+        View projects
+        </a>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default CardProj
